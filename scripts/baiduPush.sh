f123a1a0f7fcf3e8230d2789ca8a57bb56b86d2d
roshin <1425258785@qq.com>
2021-03-12 15:55:51 +0800

    ci: 百度自动推送

--- scripts/baiduPush.sh
+++ scripts/baiduPush.sh
@@ -4,7 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @temp/g.extend.roshin.cn.txt "http://data.zz.baidu.com/urls?site=https://g.extend.roshin.cn&token=ROK9cgog73ab4l49"
 curl -H 'Content-Type:text/plain' --data-binary @temp/extend.roshin.cn.txt "http://data.zz.baidu.com/urls?site=https://extend.roshin.cn&token=ROK9cgog73ab4l49"
 
 rm -rf ./temp # 删除文件
