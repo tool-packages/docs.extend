731dc62dfbbe33b1263dc329b4b2437700ba7bb1
roshin <1425258785@qq.com>
2021-03-15 17:00:18 +0800

    ci: 更改百度推送频次

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -16,16 +16,16 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }], // favicons，资源放在 public 文件夹
     ['meta', { name: 'keywords', content: keywords.join(',') }], // 属性关键字
     ['meta', { name: 'baidu-site-verification', content: 'code-eTtIfh1bRA' }], // 百度统计的站长验证
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }] // 移动浏览器主题颜色
+    // ['meta', { name: 'theme-color', content: '#3eaf7c' }] // 移动浏览器主题颜色
     // ['link', { rel: 'icon', href: '/logo.png' }],
-    // ['link', { rel: 'manifest', href: '/manifest.json' }],
-    // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    // ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    // ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    // ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    // ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   markdown: {
     lineNumbers: false,
